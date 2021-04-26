35200524484687ac8b2780ac82b9020d6f7cecd1
Patrick Lindsay <ptricklindsay@gmail.com>
2021-04-26 15:11:36 -0700

    Add cover block

--- app/javascript/block_editor/blocks/index.js
+++ app/javascript/block_editor/blocks/index.js
@@ -33,8 +33,7 @@ import MediaUpload from '../components/media-upload';
 // import * as accordion from './be-accordion';
 import * as callout from './be-alert';
 import * as card from './be-card';
-// import * as cover from './cover';
-// import * as coverButton from './cover-button';
+import * as cover from './be-cover';
 // import * as recentPosts from './recent-posts';
 import * as contactForm from './be-contact-form';
 
@@ -218,11 +217,9 @@ export const registerBlocks = () => {
   // registerBlockType(accordion.name, accordion.settings);
   registerBlockType(callout.name, callout.settings);
   registerBlockType(card.name, card.settings);
-  // registerBlockType(recentPosts.name, recentPosts.settings);
   registerBlockType(contactForm.name, contactForm.settings);
-  //
-  // registerBlockType(cover.name, cover.settings);
-  // registerBlockType(coverButton.name, coverButton.settings);
+  registerBlockType(cover.name, cover.settings);
+  // registerBlockType(recentPosts.name, recentPosts.settings);
 
   // Register custom block styles
   registerBlockStyle( 'core/button', {
