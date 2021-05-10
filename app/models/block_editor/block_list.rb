4d5e508674baecf9850ebc80efb9a3715598fa42
Patrick Lindsay <ptricklindsay@gmail.com>
2021-05-10 15:13:07 -0700

    Reusable blocks [WIP]

--- app/models/block_editor/block_list.rb
+++ app/models/block_editor/block_list.rb
@@ -3,19 +3,22 @@ module BlockEditor
   class BlockList < ApplicationRecord
     # Associations
     belongs_to :listable, polymorphic: true, touch: true, optional: true
-
     has_many :inverse_block_list_connections, class_name: "BlockListConnection", foreign_key: "child_id"
     has_many :block_list_connections, foreign_key: 'parent_id'
-
     has_many :parents, :through => :inverse_block_list_connections
     has_many :reusable_blocks, through: :block_list_connections, source: 'child'
 
     # Callbacks
     after_save_commit :touch_parents
     after_save_commit :set_reusable_blocks
+    before_validation :set_defaults
+
+    # Validations
+    validates :name, presence: true
 
     # Scopes
     scope :reusable, -> { where(listable_id: nil, listable_type: nil) }
+    scope :search, ->(query) { where('lower(name) LIKE ?', "%#{query.downcase}%") }
 
     def reusable?
       listable_type.nil? && listable_id.nil?
@@ -28,9 +31,21 @@ module BlockEditor
     end
 
     def set_reusable_blocks
-      # TODO: parse the content to find any occurrences of reusable block and grab the IDs
-      ids = 2
-      self.reusable_block_ids = ids
+      # Find all instances of a reusable block
+      html = Nokogiri::HTML(content)
+      ids = html.xpath('//comment()').select {|comment| comment.inner_text.starts_with?(" wp:#{BlockEditor::Blocks::Reusable.name}") }.map do |block_instance|
+        block_attributes = block_instance.inner_text.split(" wp:#{BlockEditor::Blocks::Reusable.name}")[1][0...-1]
+        block_attributes = block_attributes.blank? ? {} : JSON.parse(block_attributes)
+        block_attributes['ref']
+      end
+
+      self.reusable_block_ids = ids || []
+    end
+
+    def set_defaults
+      if self.name.blank?
+        self.name = Time.now
+      end
     end
   end
 end
