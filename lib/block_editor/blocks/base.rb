61e7af91f91fb0e1db29cb70de3c5c825eb474ac
Patrick Lindsay <ptricklindsay@gmail.com>
2021-06-15 11:36:52 -0700

    Updated columns styling, update options name of locals being passed to block partial & added margin-bottom to sidebar to account for footer

--- lib/block_editor/blocks/base.rb
+++ lib/block_editor/blocks/base.rb
@@ -13,7 +13,7 @@ module BlockEditor
 
         controller.render(
           partial: "block_editor/blocks/#{name}/block",
-          locals: { collection: options },
+          locals: { options: options },
           layout: false
         )
       end
